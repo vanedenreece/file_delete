9454cb83f0245ccc95060a7e3d20276cefd78106
vanedenreece <vanedenreece@gmail.com>
2025-07-04 10:13:11 +0200

    finishing touches

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
 
-# folder is the name of the folder in which we have to perform the delete operation
+# folder is the name of the folder in which we have to perform the delete operation [ specify ]
 folder = "C/USERS/PATH/TO/ROOT/FOLDER"
 
-# days is the number of days for which we have to check whether the file is older than the specified days or not
+# days is the number of days for which we have to check whether the file is older than the specified days or not [ specify]
 days = 31
+
 
 # function to perform delete operation based on condition
 def check_and_delete(folder):
@@ -19,13 +20,16 @@ def check_and_delete(folder):
    """
    This function takes the root (downloads) folder in and deletes files only
    in the root specified folder
+   [params]: file path
+   [returns]: log file with the file paths that were deleted
+   [returns]:os object that removes files
    """
-
 
    # os.walk returns 3 things: current path, files in the current path, and folders in the current path
    for (root,dirs,files) in os.walk(folder, topdown=True):
 
        for f in files:
+            
             # temp variable to store path of the file
             file_path = os.path.join(root,f)
 
@@ -36,16 +40,15 @@ def check_and_delete(folder):
             modification_date = datetime.datetime.fromtimestamp(timestamp_of_file_modified)
 
             # find the number of days when the file was modified
-
             number_of_days = (datetime.datetime.now() - modification_date).days
             
             # adding certain document types that need to be romved (Leave .exe / .msi for apps)
-            if number_of_days > days and f.endswith((".txt",".pdf",".xlsx",".csv",".png","docx","jpg")):
+            if number_of_days > days and f.endswith((".txt",".pdf",".xlsx",".csv",".png","docx","jpg")): #specify tuple
                 os.remove(file_path)
                 logging.info(f"removed file{f}")
 
-            #check for files in folders too
-            elif number_of_days > days and file_path.split("/")[3].endswith((".dat",".zip")):
+            #check for files in subfolders too
+            elif number_of_days > days and file_path.split("/")[3].endswith((".dat",".zip")): #specify tuple
                 os.remove(file_path)
                 logging.info(f"removed file{f}")
 
